1c956bc0d4ba1bf25e741c188f37e449632a08bd
Vladislav Postolachi <postolachi.vlad@gmail.com>
2020-07-22 02:05:23 +0200

    linked logo to home page

--- landing/urls.py
+++ landing/urls.py
@@ -2,7 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="home"),
     path('step-01', step_one),
     path('step-02', step_two),
     path('step-03', step_three),
